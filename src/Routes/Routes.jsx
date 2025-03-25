146d71765f6be0de8afe2268817b74aca713d0ab
unknown <faisaljuma01@gmail.com>
2025-03-25 13:00:51 +0500

    basecode added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import {
-    createBrowserRouter,
-  } from "react-router-dom";
+  createBrowserRouter,
+} from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home";
 import AboutPage from "../Pages/AboutPage";
@@ -17,65 +17,66 @@ import ContactPage from "../Pages/ContactPage";
 import BlogDetailsPage from "../Pages/BlogDetailsPage";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-          path: "/",
-          element: <Home></Home>,
-        },
-        {
-            path: "/about",
-            element: <AboutPage></AboutPage>,
-        },
-        {
-          path: "/service",
-          element: <ServicesPage></ServicesPage>,
-        },
-        {
-          path: "/service/service-details",
-          element: <ServiceDetailsPage></ServiceDetailsPage>,
-        }, 
-        {
-          path: "/project",
-          element: <ProjectPage></ProjectPage>,
-        },      
-        {
-          path: "/project/project-details",
-          element: <ProjectDetailsPage></ProjectDetailsPage>,
-        },
-        {
-          path: "/team",
-          element: <TeamPage></TeamPage>,
-        },
-        {
-          path: "/team/team-details",
-          element: <TeamDetailsPage></TeamDetailsPage>,
-        },
-        {
-          path: "/blog",
-          element: <BlogPage></BlogPage>,
-        },
-        {
-          path: "/blog/blog-details",
-          element: <BlogDetailsPage></BlogDetailsPage>,
-        },        
-        {
-          path: "/contact",
-          element: <ContactPage></ContactPage>,
-        },
-
-      ],
-    }, 
-    {
-      path: 'home2',
-      element: <Layout2></Layout2>,
-      children: [
-        {
-          index: true,
-          element: <Home2></Home2>,
-        },                           
-      ],
-    },          
-  ]);
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+          path: "/about",
+          element: <AboutPage></AboutPage>,
+      },
+      {
+        path: "/service",
+        element: <ServicesPage></ServicesPage>,
+      },
+      {
+        path: "/service/service-details",
+        element: <ServiceDetailsPage></ServiceDetailsPage>,
+      }, 
+      {
+        path: "/project",
+        element: <ProjectPage></ProjectPage>,
+      },      
+      {
+        path: "/project/project-details",
+        element: <ProjectDetailsPage></ProjectDetailsPage>,
+      },
+      {
+        path: "/team",
+        element: <TeamPage></TeamPage>,
+      },
+      {
+        path: "/team/team-details",
+        element: <TeamDetailsPage></TeamDetailsPage>,
+      },
+      {
+        path: "/blog",
+        element: <BlogPage></BlogPage>,
+      },
+      {
+        path: "/blog/blog-details",
+        element: <BlogDetailsPage></BlogDetailsPage>,
+      },        
+      {
+        path: "/contact",
+        element: <ContactPage></ContactPage>,
+      },
+    ],
+  }, 
+  {
+    path: 'home2',
+    element: <Layout2></Layout2>,
+    children: [
+      {
+        index: true,
+        element: <Home2></Home2>,
+      },                           
+    ],
+  }        
+], {
+  basename: "/fajservicess"
+});
