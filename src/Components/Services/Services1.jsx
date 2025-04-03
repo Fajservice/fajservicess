feedfaca229093a96953272be65ca91bd702e8aa
Waseem Hassan <waseem.hassan@codeavour.com>
2025-04-03 13:39:36 +0400

    some fixes

--- src/Components/Services/Services1.jsx
+++ src/Components/Services/Services1.jsx
@@ -77,7 +77,7 @@ const Services1 = () => {
                     <div className="cs_card_icon cs_center cs_heading_bg cs_mb_22">
                       <img src={`${import.meta.env.BASE_URL}${item.icon}`} alt="Service Icon" />
                     </div>
-                    <h3 className="cs_card_title cs_fs_24 cs_mb_8">
+                    <h3 className="cs_card_title cs_fs_20 cs_mb_8">
                       <Link to={item.btnLink}>{item.title}</Link></h3>
                     <p className="cs_card_subtitle cs_mb_18">{item.desc}</p>
                     <Link to={item.btnLink} className="cs_text_btn cs_style_1 cs_bold cs_heading_color">
