4f4631fdc25122bf53a5416b1350616bfe69e0ac
waqarfaj123 <faj70753@gmail.com>
2025-04-16 10:11:06 +0500

    button components update

--- src/Components/Buttons/GetQuoteButton.jsx
+++ src/Components/Buttons/GetQuoteButton.jsx
@@ -1,10 +1,16 @@
 // src/components/Button/GetQuoteButton.jsx
-import React from 'react';
+// import React from 'react';
+import React, { useState } from 'react';
+
 const GetQuoteButton = ({ whatsapp = 'https://api.whatsapp.com/send?phone=+971507464712&text=Hello' }) => {
+    const [hovered, setHovered] = useState(false);
     return (
-        <a href={whatsapp} className="quote-now-btn">
+        <a href={whatsapp} className="quote-now-btn"
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}>
             <img
                 className="quote-icon"
+                src={`${import.meta.env.BASE_URL}img/icons/${hovered ? 'quote-icon-hover.png' : 'quote-icon.png'}`}
                 alt="Get A Quote"
             />
         </a>
