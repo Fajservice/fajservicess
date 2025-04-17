b489f0837a5ddbadfc727273597a10d1e09c91c6
Waseem Hassan <waseem.hassan@codeavour.com>
2025-04-17 13:40:15 +0400

    coleman and Yorl Page

--- src/Pages/ColemenAcRepair.jsx
+++ src/Pages/ColemenAcRepair.jsx
@@ -5,7 +5,7 @@ const ColemenAcRepair = () => {
     return (
         <div className="serviceDetails">
             <BreadCumb
-                bgImg="/img/page_heading_1.jpg"
+                bgImg="/img/ac_service-bannerbg.jpg"
                 // bgImg="/img/ac_service-bannerbg.jpg"
                 Title="Coleman AC Repair in Dubai - Central AC Service"
             ></BreadCumb> 
--- src/Pages/YorkAcRepair.jsx
+++ src/Pages/YorkAcRepair.jsx
@@ -5,7 +5,7 @@ const YorkAcRepair = () => {
     return (
         <div className="serviceDetails">
             <BreadCumb
-                bgImg="/img/page_heading_1.jpg"
+                bgImg="/img/ac_service-bannerbg.jpg"
                 // bgImg="/img/ac_service-bannerbg.jpg"
                 Title="York AC Repair in Dubai - Air Conditioning Maintenance Near Me"
             ></BreadCumb> 
