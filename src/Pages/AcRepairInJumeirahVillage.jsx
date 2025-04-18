0e6dd01309d9a9d1f9d3c4f7ea41166335149330
unknown <faisaljuma01@gmail.com>
2025-04-18 17:16:23 +0500

    update

--- src/Pages/AcRepairInJumeirahVillage.jsx
+++ src/Pages/AcRepairInJumeirahVillage.jsx
@@ -7,7 +7,7 @@ const AcRepairInJumeirahVillage = () => {
     <div className="serviceDetails">
       <BreadCumb
         // bgImg="/img/page_heading_1.jpg"
-        bgImg="/img/ac_service-bannerbg.jpg"
+        bgImg="/img/air-condition.jpg"
         Title="AC Service in Jumeirah Village Dubai"
       ></BreadCumb>
       <AcRepairInJumeirahVillageDetail />
