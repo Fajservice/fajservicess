d82b1dc98b458263c03337832a130d4b8ebbae8b
waqarfaj123 <faj70753@gmail.com>
2025-04-19 15:03:08 +0500

    content changes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -62,7 +62,7 @@ export const router = createHashRouter([
       {
         path: "/ac-annual-maintenance-contract/",
         element: <AcAnnualMaintenanceContract></AcAnnualMaintenanceContract>,
-      },      
+      },
       {
         path: "/ac-repair-dubai/",
         element: <AcRepairDubai></AcRepairDubai>,
@@ -110,7 +110,7 @@ export const router = createHashRouter([
       {
         path: "/project",
         element: <ProjectPage></ProjectPage>,
-      },      
+      },
       {
         path: "/project/project-details",
         element: <ProjectDetailsPage></ProjectDetailsPage>,
