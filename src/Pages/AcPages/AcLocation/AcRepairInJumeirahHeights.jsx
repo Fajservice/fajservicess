9167fd85548f90b11234136c231619b5bd2f2460
unknown <faisaljuma01@gmail.com>
2025-05-02 16:35:36 +0500

    location updated

---
+++ src/Pages/AcPages/AcLocation/AcRepairInJumeirahHeights.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import BreadCumb from "../../../Components/Common/BreadCumb";
+import AcRepairInJumeirahHeightsDetail from '../../../Components/ServiceDetails/AcDetail/AcLocation/AcRepairInJumeirahHeightsDetail';
+
+const AcRepairInJumeirahHeights = () => {
+    return (
+        <div className="serviceDetails">
+        <BreadCumb
+            // bgImg="/img/page_heading_1.jpg"
+            bgImg="/img/air-condition.jpg"
+            Title="AC Service and Repair in Jumeirah Islands"
+        ></BreadCumb>
+        <AcRepairInJumeirahHeightsDetail />
+    </div>
+      )
+}
+
+export default AcRepairInJumeirahHeights
---
+++ src/Pages/AcPages/AcLocation/AcRepairInQuozIndustrialArea.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import BreadCumb from "../../../Components/Common/BreadCumb";
+const AcRepairInQuozIndustrialArea = () => {
+  return (
+    <div className="serviceDetails">
+    <BreadCumb
+        // bgImg="/img/page_heading_1.jpg"
+        bgImg="/img/air-condition.jpg"
+        Title="AC Service and Repair in Jumeirah Islands"
+    ></BreadCumb>
+    <AcRepairInJumeirahIslandsDetail />
+</div>
+  )
+}
+
+export default AcRepairInQuozIndustrialArea
