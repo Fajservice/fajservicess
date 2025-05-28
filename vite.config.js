1b35ea9bae6e9af015258845af0ddcd31d19fed2
unknown <faisaljuma01@gmail.com>
2025-05-28 17:29:23 +0500

    optimize vite

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,23 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   //  base: '/dist/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          ui: ['@mui/material', 'antd']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
 })
