dd865a4dece034d4a88818fab38833fd596a0807
waqarfaj123 <faj70753@gmail.com>
2025-05-28 17:26:05 +0500

    Brandwise App SEO Tags

--- src/Components/ServiceDetails/AppliancesDetail/AppliancesBrand/FisherPaykelFridgeRepairDetail.jsx
+++ src/Components/ServiceDetails/AppliancesDetail/AppliancesBrand/FisherPaykelFridgeRepairDetail.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Link, parsePath } from "react-router-dom";
 import data from '../../../../Data/AppliancesData/AppliancesFaqs/AppliancesFaqs.json';
-import { HelmetProvider } from "react-helmet-async";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import Serviceappointemnt from '../../../Contact/Serviceappointemnt';
 
 import WhatsappIconButton from "../../../Buttons/WhatsappIconButton";
@@ -14,17 +14,16 @@ import parse from 'html-react-parser';
 import HeaderForm from "../../../Headeform/HeaderForm";
 import AppliancesAppointmentCol from "../../../ApplianceCommons/AppliancesAppointmentCol";
 
-const FisherPaykelFridgeRepairDetail = ({ subtitle, title, bgImg }) => {
+const FisherPaykelFridgeRepairDetail = ({ subtitle, title, reviewsbg, titleSeo, description, Author, Keyword, URL }) => {
   // For SEO
-  const titleSeo = "Fisher and Paykel Fridge Repair & Dishwasher Services Dubai";
-  const description = "Fisher and Paykel Fridge Repair Dubai. Get expert Fisher & Paykel refrigerator, washing machine service, dishwasher, stove, and oven repairs near me";
-  const Keyword = "";
-  const Author = "Faj Technical Servcies";
-  const URL = "https://www.fajservices.com/fisher-and-paykel-fridge-repair-in-dubai-fisher-and-paykel-washing-machine-repair-in-dubai-fisher-and-paykel-cooker-repair-in-dubai-fisher-and-paykel-oven-repair-in-dubai-fisher-and-paykel-appliances/";
-
+  const metatitle = String(titleSeo || "Fisher and Paykel Fridge Repair & Dishwasher Services Dubai");
+  const metadescription = String(description || "Fisher and Paykel Fridge Repair Dubai. Get expert Fisher & Paykel refrigerator, washing machine service, dishwasher, stove, and oven repairs near me");
+  const metaAuthor = String(Author || "Faj Technical Services");
+  const metaKeyword = String(Keyword || "Fisher and Paykel home appliances repair, Fridge repair services in dubai");
+  const metaURL = String(URL || "https://www.fajservices.ae/fisher-and-paykel-fridge-repair-in-dubai-fisher-and-paykel-washing-machine-repair-in-dubai-fisher-and-paykel-cooker-repair-in-dubai-fisher-and-paykel-oven-repair-in-dubai-fisher-and-paykel-appliances/");
   subtitle = "Testimonial"
   title = "What our clients say About Us"
-  bgImg = "img/testimonialbg.jpg"
+  reviewsbg = "img/testimonialbg.jpg"
   const accordionContentRef = useRef(null);
   const [openItemIndex, setOpenItemIndex] = useState(-1);
   const [firstItemOpen, setFirstItemOpen] = useState(true);
@@ -113,21 +112,21 @@ const FisherPaykelFridgeRepairDetail = ({ subtitle, title, bgImg }) => {
   };
   return (
     <>
+
       <HelmetProvider>
-        <title>{titleSeo}</title>
-        <meta name="description" content={description}></meta>
-        <meta name="keywords" content={Keyword} />
-        <meta name="author" content={Author} />
-        <meta name="robots" content="index, follow" />
-
-        <Link rel="canonical" href={titleSeo} />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:title" content={titleSeo} />
-        <meta property="og:description" content={description} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={URL} />
-
+        <Helmet>
+          <title>{metatitle}</title>
+          <meta name="description" content={metadescription}></meta>
+          <meta name="keywords" content={metaKeyword} />
+          <meta name="author" content={metaAuthor} />
+          <meta name="robots" content="index, follow" />
+          <link rel="canonical" href={metaURL} />
+          <meta property="og:type" content="website" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:title" content={metatitle} />
+          <meta property="og:description" content={metadescription} />
+          <meta property="og:url" content={metaURL} />
+        </Helmet>
       </HelmetProvider>
       <HeaderForm />
 
@@ -477,11 +476,11 @@ const FisherPaykelFridgeRepairDetail = ({ subtitle, title, bgImg }) => {
           </div>
         </section>
 
-        
+
         {/* Maintenance Contract */}
         <MaintenanceContract />
-{/* testimobial section */}
-        <section className="cs_slider cs_style_1 cs_slider_gap_30 cs_bg_filed position-relative cs_py_30" style={{ backgroundImage: `url(${import.meta.env.BASE_URL}${bgImg})` }}>
+        {/* testimobial section */}
+        <section className="cs_slider cs_style_1 cs_slider_gap_30 cs_bg_filed position-relative cs_py_30" style={{ backgroundImage: `url(${import.meta.env.BASE_URL}${reviewsbg})` }}>
           {/* <div className="cs_height_80 cs_height_lg_80"></div> */}
           <div className="container">
             <div className="cs_section_heading cs_style_1 cs_mb_47 text-center">
--- src/Components/ServiceDetails/AppliancesDetail/AppliancesBrand/FrankeAppliancesRepairServiceDetail.jsx
+++ src/Components/ServiceDetails/AppliancesDetail/AppliancesBrand/FrankeAppliancesRepairServiceDetail.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Link, parsePath } from "react-router-dom";
 import data from '../../../../Data/AppliancesData/AppliancesFaqs/AppliancesFaqs.json';
-import { HelmetProvider } from "react-helmet-async";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import Serviceappointemnt from '../../../Contact/Serviceappointemnt';
 
 import WhatsappIconButton from "../../../Buttons/WhatsappIconButton";
@@ -14,17 +14,16 @@ import parse from 'html-react-parser';
 import HeaderForm from "../../../Headeform/HeaderForm";
 import AppliancesAppointmentCol from "../../../ApplianceCommons/AppliancesAppointmentCol";
 
-const FrankeAppliancesRepairServiceDetail = ({ subtitle, title, bgImg }) => {
+const FrankeAppliancesRepairServiceDetail = ({ subtitle, title, reviewsbg, titleSeo, description, Author, Keyword, URL }) => {
   // For SEO
-  const titleSeo = "Franke Appliances Repair – Dishwasher & Oven Services";
-  const description = "Franke Appliances Repair Dubai. Get Best Franke fridge, refrigerator fix, washing machine, cooker, oven repair service near me in Dubai Call 043300002";
-  const Keyword = "";
-  const Author = "Faj Technical Servcies";
-  const URL = "https://www.fajservices.com/franke-appliances-repair-service-in-dubai/";
-
+  const metatitle = String(titleSeo || "Franke Appliances Repair – Dishwasher & Oven Services");
+  const metadescription = String(description || "Franke Appliances Repair Dubai. Get Best Franke fridge, refrigerator fix, washing machine, cooker, oven repair service near me in Dubai Call 043300002");
+  const metaAuthor = String(Author || "Faj Technical Services");
+  const metaKeyword = String(Keyword || "Franke Dishwasher repair, Franke home appliances services");
+  const metaURL = String(URL || "https://www.fajservices.ae/franke-appliances-repair-service-in-dubai/");
   subtitle = "Testimonial"
   title = "What our clients say About Us"
-  bgImg = "img/testimonialbg.jpg"
+  reviewsbg = "img/testimonialbg.jpg"
   const accordionContentRef = useRef(null);
   const [openItemIndex, setOpenItemIndex] = useState(-1);
   const [firstItemOpen, setFirstItemOpen] = useState(true);
@@ -114,19 +113,19 @@ const FrankeAppliancesRepairServiceDetail = ({ subtitle, title, bgImg }) => {
   return (
     <>
       <HelmetProvider>
-        <title>{titleSeo}</title>
-        <meta name="description" content={description}></meta>
-        <meta name="keywords" content={Keyword} />
-        <meta name="author" content={Author} />
-        <meta name="robots" content="index, follow" />
-        <Link rel="canonical" href={titleSeo} />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:title" content={titleSeo} />
-        <meta property="og:description" content={description} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={URL} />
-
+        <Helmet>
+          <title>{metatitle}</title>
+          <meta name="description" content={metadescription}></meta>
+          <meta name="keywords" content={metaKeyword} />
+          <meta name="author" content={metaAuthor} />
+          <meta name="robots" content="index, follow" />
+          <link rel="canonical" href={metaURL} />
+          <meta property="og:type" content="website" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:title" content={metatitle} />
+          <meta property="og:description" content={metadescription} />
+          <meta property="og:url" content={metaURL} />
+        </Helmet>
       </HelmetProvider>
       <HeaderForm />
 
@@ -476,11 +475,11 @@ const FrankeAppliancesRepairServiceDetail = ({ subtitle, title, bgImg }) => {
           </div>
         </section>
 
-        
+
         {/* Maintenance Contract */}
         <MaintenanceContract />
-{/* testimobial section */}
-        <section className="cs_slider cs_style_1 cs_slider_gap_30 cs_bg_filed position-relative cs_py_30" style={{ backgroundImage: `url(${import.meta.env.BASE_URL}${bgImg})` }}>
+        {/* testimobial section */}
+        <section className="cs_slider cs_style_1 cs_slider_gap_30 cs_bg_filed position-relative cs_py_30" style={{ backgroundImage: `url(${import.meta.env.BASE_URL}${reviewsbg})` }}>
           {/* <div className="cs_height_80 cs_height_lg_80"></div> */}
           <div className="container">
             <div className="cs_section_heading cs_style_1 cs_mb_47 text-center">
