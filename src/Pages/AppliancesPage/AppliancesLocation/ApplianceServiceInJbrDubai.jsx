8e6379f24316c142983503ebde9d5bc164dffd07
Waseem Hassan <faisaljuma01@gmail.com>
2025-07-09 09:35:32 +0400

    price section updated in brandwise appliances jbr

--- src/Pages/AppliancesPage/AppliancesLocation/ApplianceServiceInJbrDubai.jsx
+++ src/Pages/AppliancesPage/AppliancesLocation/ApplianceServiceInJbrDubai.jsx
@@ -1,15 +1,17 @@
 import BreadCumb from "../../../Components/Common/BreadCumb";
-import ApplianceServiceInJbrDubaiDetail from "../../../Components/ServiceDetails/AppliancesDetail/AppliancesLocation/ApplianceServiceInJbrDubaidetail";
+import ApplianceServiceInJbrDetail from "../../../Components/ServiceDetails/AppliancesDetail/AppliancesLocation/ApplianceServiceInJbrDetail";
 const ApplianceServiceInJbrDubai = () => {
   return (
+    <>
     <div className="serviceDetails">
       <BreadCumb
         bgImg="img/banners/appliances-banner-desktop-faj.avif"
         mobileBgImg="img/banners/appliances-banner-mobile-faj.avif"
         Title="Home Appliance Service in Jbr Dubai">
       </BreadCumb>
-      <ApplianceServiceInJbrDubaiDetail />
     </div>
+    <ApplianceServiceInJbrDetail />
+    </>
   )
 }
 
