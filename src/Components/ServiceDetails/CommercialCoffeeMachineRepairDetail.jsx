66b0e0a8fb2c786a1b1a22c49548fdcdbb1170a4
Waseem Hassan <faisaljuma01@gmail.com>
2025-07-09 10:04:33 +0400

    coffee machine

--- src/Components/ServiceDetails/CommercialCoffeeMachineRepairDetail.jsx
+++ src/Components/ServiceDetails/CommercialCoffeeMachineRepairDetail.jsx
@@ -585,6 +585,11 @@ const CommercialCoffeeMachineRepairDetail = ({ subtitle, title, reviewsbg, title
 
         <section className="section cs_py_30 bg-light-gray custom-css-box">
           <div className="container">
+            <h3 className="mb-1 pt-3">EXPRESS & EMERGENCY Coffee MAchine REPAIR SERVICE</h3>
+            <p className="mb-2"><b>Terms & Conditions:</b> There is a callout fee that applies, ranging from AED 157 to 280 depending on capacity, for each diagnosis. Same-day visits are available for bookings made before *12:00 PM. For bookings made after 12:00 PM, next-day visits may be arranged, subject to availability.</p>
+            <p className="pt-3 border-small-top"><strong>CHOOSE FAJ FOR YOUR PEACE OF MIND</strong><br />
+            <b>We provide 2-month repair warranty</b><br />
+            and <small>3-month parts warranty</small> as standard.</p>
             <h3 className="text-center">CHOOSE US FOR RELIABLE, EXCEPTIONAL SERVICE TAILORED TO YOUR NEEDS!</h3>
             <div className="row gx-2 gx-lg-3 gy-3 gy-lg-4 justify-content-center">
 
