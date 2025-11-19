eea14edba42029d8b8d49e1c23bcfc1c092ba100
Waseem Hassan <faisaljuma01@gmail.com>
2025-11-19 13:34:37 +0400

    oven galleryies

--- src/Components/ServiceDetails/AppliancesDetail/AppliancesBrand/IfbRefrigeratorRepairDetail.jsx
+++ src/Components/ServiceDetails/AppliancesDetail/AppliancesBrand/IfbRefrigeratorRepairDetail.jsx
@@ -24,6 +24,7 @@ const IfbRefrigeratorRepairDetail = ({ subtitle, title, reviewsbg, titleSeo, des
   const metadescription = String(description || "We are offering IFB refrigerator repair in Dubai Or fridge, washing machine, cooking range, dishwasher service center in Dubai, Freezer service in Dubai Cal us now");
   const metaImage = String(Image || "https://www.fajservices.ae/img/The-Most-Common-Reasons-for-Appliance-Breakdowns.avif");
   const metaKeyword = String(Keyword || "IFB Refrigerator Repair, IFB Washing Machine Repair, IFB Cooker Repair, IFB Dishwasher Repair, IFB Oven Repair, IFB Freezer Service in Dubai");
+  const metaAuthor = String(Author || "FAJ Technical Services L.L.C");
   const metaURL = String(URL || "https://www.fajservices.ae/ifb-refrigerator-repair-in-dubai-fridge-repair-in-dubai-washing-machine-repair-in-dubai-dishwasher-cooker-oven-repairs-fix-service-in-dubai/").replace(/\/?$/, '/');
 
   subtitle = "Testimonial"
