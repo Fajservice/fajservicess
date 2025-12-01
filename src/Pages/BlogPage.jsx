9a485a3e0f8b3d01b50919ccac7aea9e0ac3a989
Waseem Hassan <faisaljuma01@gmail.com>
2025-12-01 16:46:08 +0400

    some fixesd

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -25,23 +25,7 @@ const BlogPage = ({ titleSeo, description, Author, Keyword, URL }) => {
                     <meta property="og:url" content={metaURL} />
                   </Helmet>
                    <div>
-            <section
-                className="cs_page_heading cs_bg_filed cs_primary_bg"
-                style={{ backgroundImage: `url(${import.meta.env.BASE_URL}img/page_heading_1.avif)` }}
-                >
-                <div className="container h-100">
-                    {/* Main row */}
-                    <div className="row align-items-center h-100">
-                        <div className="col-md-12 d-flex justify-content-start align-items-center">
-                            <h1 className="cs_white_color text-left offset-md-1" style={{ fontSize: "clamp(1.75rem, 5vw, 2.625rem)" }}>
-                                Blogs
-                            </h1>
-                        </div>
-                    {/* Form section */}
-                    </div>
-                </div>
-                <div className="pb-3 d-md-none"></div>
-            </section> 
+            
             
             <section className="section cs_py_30">
                 <div className="container">
--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -16,34 +16,35 @@ const ContactPage = ({titleSeo, description, Author, Keyword, URL }) => {
     return (
         <>
             <HelmetProvider>
-                           <Helmet>
-                               <title>{metatitle}</title>
-                               <meta name="description" content={metadescription} />
-                               <meta name="keywords" content={metaKeyword} />
-                               <meta name="author" content={metaAuthor} />
-                               <meta name="robots" content="index, follow" />
-           
-                               <link rel="canonical" href={metaURL} />
-                               <meta property="og:type" content="website" />
-                               <meta property="og:locale" content="en_US" />
-                               <meta property="og:title" content={metatitle} />
-                               <meta property="og:description" content={metadescription} />
-                               <meta property="og:url" content={metaURL} />
-                               <meta property="og:image" content={metaImage} />
-           
-                               {/* Twitter Card */}
-                               <meta name="twitter:card" content="summary_large_image" />
-                               <meta name="twitter:title" content={metatitle} />
-                               <meta name="twitter:description" content={metadescription} />
-                               <meta name="twitter:image" content={metaImage} />
-                               <meta name="twitter:url" content={metaURL} />
-                           </Helmet>
-                       </HelmetProvider>
+                <Helmet>
+                    <title>{metatitle}</title>
+                    <meta name="description" content={metadescription} />
+                    <meta name="keywords" content={metaKeyword} />
+                    <meta name="author" content={metaAuthor} />
+                    <meta name="robots" content="index, follow" />
+
+                    <link rel="canonical" href={metaURL} />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:locale" content="en_US" />
+                    <meta property="og:title" content={metatitle} />
+                    <meta property="og:description" content={metadescription} />
+                    <meta property="og:url" content={metaURL} />
+                    <meta property="og:image" content={metaImage} />
+
+                    {/* Twitter Card */}
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:title" content={metatitle} />
+                    <meta name="twitter:description" content={metadescription} />
+                    <meta name="twitter:image" content={metaImage} />
+                    <meta name="twitter:url" content={metaURL} />
+                </Helmet>
+            </HelmetProvider>
         <div>
             <BreadCumb
                 bgImg="img/page_heading_1.avif"
                 Title="Contact"
             ></BreadCumb> 
+            <h1 className="cs_fs_30 text-center mb-0 pt-5">Contact Us</h1>
             <Contact1
                 Title="Contact Information"
                 subTitle="FAJ location is easily accessible from Al Khail Road and Sheikh Zayed Road. Drop off your appliance at the workshop to receive a discount on the technical inspection fee and service.."
