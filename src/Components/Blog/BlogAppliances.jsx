7ace1623a14caec03153d5c1c9636a0b06cb113b
bilal <martincharles775@gmail.com>
2026-02-20 14:44:20 +0500

    home coverage and brands page

--- src/Components/Blog/BlogAppliances.jsx
+++ src/Components/Blog/BlogAppliances.jsx
@@ -31,7 +31,7 @@ const BlogAppliances = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${import.meta.env.BASE_URL}data/appliancesblog.json`);
+        const response = await fetch(`${import.meta.env.BASE_URL}data/coveragebloghomeapplaince.json`);
         const jsonData = await response.json();
         setData(jsonData);
       } catch (error) {
