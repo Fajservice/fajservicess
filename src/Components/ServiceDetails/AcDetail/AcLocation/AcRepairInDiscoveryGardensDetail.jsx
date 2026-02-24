8a565d112a2bf8601ddde79f66258538d21e0b6a
bilal <martincharles775@gmail.com>
2026-02-24 09:33:17 +0500

    link updated ac

--- src/Components/ServiceDetails/AcDetail/AcLocation/AcRepairInDiscoveryGardensDetail.jsx
+++ src/Components/ServiceDetails/AcDetail/AcLocation/AcRepairInDiscoveryGardensDetail.jsx
@@ -15,12 +15,14 @@ import GetQuoteButton from "../../../Buttons/GetQuoteButton";
 import LocationKeyword from "./LocationKeyword";
 import Testimonial1 from "../../../Testimonial/Testimonial1";
 import PriceCard from "./PriceCard";
+import AcBenefits from "../AcBenefit";
+import BeforeAfter from "../../../BeforeAfter/BeforeAfter";
 const CDN = 'https://imagedelivery.net/7jVKF8FS0aEmjeSSRZqLyA';
 
 const getImageSrc = (imgPath) => {
-  if (!imgPath) return '';
-  if (imgPath.startsWith('https')) return imgPath;
-  return `${CDN}/${imgPath}/public`;
+   if (!imgPath) return '';
+   if (imgPath.startsWith('https')) return imgPath;
+   return `${CDN}/${imgPath}/public`;
 };
 
 const AcRepairInDiscoveryGardensDetail = ({ subtitle, title, reviewsbg, titleSeo, description, Author, Keyword, URL }) => {
@@ -40,51 +42,51 @@ const AcRepairInDiscoveryGardensDetail = ({ subtitle, title, reviewsbg, titleSeo
    const [firstItemOpen, setFirstItemOpen] = useState(true);
 
    // State for fetched data
-           const [data, setData] = useState([]);
-           const [testimonial_data, setTestimonialData] = useState([]);
-           const [isLoading, setIsLoading] = useState(true);
-       
-           const handleItemClick = index => {
-               if (index === openItemIndex) {
-                   setOpenItemIndex(-1);
-               } else {
-                   setOpenItemIndex(index);
-               }
-           };
-           useEffect(() => {
-               if (firstItemOpen) {
-                   setOpenItemIndex(0);
-                   setFirstItemOpen(false);
-               }
-           }, [firstItemOpen]);
-       
-           useEffect(() => {
-               loadBackgroudImages();
-           }, []);
-       
-           // Fetch JSON data
-           useEffect(() => {
-               const fetchData = async () => {
-                   try {
-                       const [faqsResponse, testimonialsResponse] = await Promise.all([
-                           fetch(`${import.meta.env.BASE_URL}data/AcData/AcFaqs/AcLocation/AcRepairInAlBadaa.json`),
-                           fetch(`${import.meta.env.BASE_URL}data/AcData/AcTestimonial/AcServiceTestimonials.json`)
-                       ]);
-       
-                       const faqsData = await faqsResponse.json();
-                       const testimonialsData = await testimonialsResponse.json();
-       
-                       setData(faqsData);
-                       setTestimonialData(testimonialsData);
-                   } catch (error) {
-                       console.error('Error fetching data:', error);
-                   } finally {
-                       setIsLoading(false);
-                   }
-               };
-       
-               fetchData();
-           }, []);
+   const [data, setData] = useState([]);
+   const [testimonial_data, setTestimonialData] = useState([]);
+   const [isLoading, setIsLoading] = useState(true);
+
+   const handleItemClick = index => {
+      if (index === openItemIndex) {
+         setOpenItemIndex(-1);
+      } else {
+         setOpenItemIndex(index);
+      }
+   };
+   useEffect(() => {
+      if (firstItemOpen) {
+         setOpenItemIndex(0);
+         setFirstItemOpen(false);
+      }
+   }, [firstItemOpen]);
+
+   useEffect(() => {
+      loadBackgroudImages();
+   }, []);
+
+   // Fetch JSON data
+   useEffect(() => {
+      const fetchData = async () => {
+         try {
+            const [faqsResponse, testimonialsResponse] = await Promise.all([
+               fetch(`${import.meta.env.BASE_URL}data/AcData/AcFaqs/AcLocation/AcRepairInAlBadaa.json`),
+               fetch(`${import.meta.env.BASE_URL}data/AcData/AcTestimonial/AcServiceTestimonials.json`)
+            ]);
+
+            const faqsData = await faqsResponse.json();
+            const testimonialsData = await testimonialsResponse.json();
+
+            setData(faqsData);
+            setTestimonialData(testimonialsData);
+         } catch (error) {
+            console.error('Error fetching data:', error);
+         } finally {
+            setIsLoading(false);
+         }
+      };
+
+      fetchData();
+   }, []);
 
    return (
       <>
@@ -107,7 +109,7 @@ const AcRepairInDiscoveryGardensDetail = ({ subtitle, title, reviewsbg, titleSeo
                <meta name="twitter:title" content={metatitle} />
                <meta name="twitter:description" content={metadescription} />
                <meta name="twitter:image" content={metaImage} />
-               
+
             </Helmet>
          </HelmetProvider>
          <HeaderForm />
@@ -130,15 +132,15 @@ const AcRepairInDiscoveryGardensDetail = ({ subtitle, title, reviewsbg, titleSeo
             <PriceCard />
 
             {/* Experts AC Service and Maintenance */}
-            <section className="section cs_py_30 bg-light-gray">
+            <section className="section cs_py_30 bg-light-white">
                <div className="container">
-                  <div className="row gx-md-5">
+                  <div className="row gx-md-5 expert_section_wrapper">
                      <div className="col-md-6">
-                        <h2 className="cs_fs_24 mb-1">Best AC Repair in Discovery Gardens</h2>
+                        <h2 className="cs_fs_24 mb-1 text-align-left">Best AC Repair in Discovery Gardens</h2>
                         <p className="mb-2">When it comes to <Link to="/services/air-conditioning-repair/ac-service/"><b>AC repair</b></Link> near me in Discovery Gardens, it’s essential to hire a professional and trustworthy <Link to="/services/air-conditioning-repair/ac-service/"><b>AC technician</b></Link> near me for your home AC or office AC.</p>
 
                         <p>We specialise in air conditioner (AC) repair, service, and maintenance near me in Discovery Gardens Dubai. The <a href="https://www.facebook.com/FAJTechnicalServicesLLC">FAJ team</a> is dedicated to ensuring a comfortable temperature all year round.</p>
-                        <h2 className="cs_fs_24 mb-1 pt-3 border-small-top">Signs You Need An AC Service</h2>
+                        <h2 className="cs_fs_24 text-align-left">Signs You Need An AC Service</h2>
                         <p className="mb-2">If you notice any of these signs, please contact us via WhatsApp. We are here to assist your AC cooling system in achieving optimal efficiency.</p>
                         <div className="row">
                            <div className="col-sm-6">
@@ -158,13 +160,15 @@ const AcRepairInDiscoveryGardensDetail = ({ subtitle, title, reviewsbg, titleSeo
                      </div>
 
                      <div className="col-md-6 ">
-                        <img className="bordered-img w-100" src={getImageSrc('Experts-AC-Service-and-Maintenance')} alt="AC Repair in Discovery Gardens"  />
+                        <img className="bordered-img w-100" src={getImageSrc('Experts-AC-Service-and-Maintenance')} alt="AC Repair in Discovery Gardens" />
 
                      </div>
                   </div>
 
                   <div className="appointment-col border-small-top pt-3">
-                     <p>If you require a same-day visit, please book before 12 PM. Appointments made after 12 PM will be scheduled for the next day based on availability.<b /> <b />For technical inspection / callout of <Link to="/services/air-conditioning-repair/ac-service/"><b>AC cleaning service</b></Link>, air conditioning repair, or AC maintenance near me in Discovery Gardens. Please click below. To book an appointment, you can call or reach us on WhatsApp at +971 50 746 4712.</p>
+                     <div className="expert_section_wrapper px-3">
+                        <p>If you require a same-day visit, please book before 12 PM. Appointments made after 12 PM will be scheduled for the next day based on availability.<b /> <b />For technical inspection / callout of <Link to="/services/air-conditioning-repair/ac-service/"><b>AC cleaning service</b></Link>, air conditioning repair, or AC maintenance near me in Discovery Gardens. Please click below. To book an appointment, you can call or reach us on WhatsApp at +971 50 746 4712.</p>
+                     </div>
                      <div id="get-quote" className=" mt-3">
                         <div className="container d-flex justify-content-center align-items-center gap-3">
                            <WhatsappIconButton />
@@ -188,7 +192,7 @@ const AcRepairInDiscoveryGardensDetail = ({ subtitle, title, reviewsbg, titleSeo
 
                   <div className="row align-items-center">
                      <div className="col-md-6">
-                        <img className="blue-border" src={getImageSrc('What-is-covered-in-an-AC-Maintenance-Contract')} alt="Air Conditioning Service in Discovery Gardens"  />
+                        <img className="blue-border" src={getImageSrc('What-is-covered-in-an-AC-Maintenance-Contract')} alt="Air Conditioning Service in Discovery Gardens" />
                      </div>
                      <div className="col-md-6">
                         <ul className="mb-0">
@@ -352,80 +356,7 @@ const AcRepairInDiscoveryGardensDetail = ({ subtitle, title, reviewsbg, titleSeo
             </section>
 
             {/* The Benefits Of Regular AC Service*/}
-            <section className="section cs_py_30 ac_benifit_blue">
-               <div className="container">
-                  <div className="row justify-content-center">
-                     <div className="col-lg-10 text-center">
-                        <h2 className="cs_fs_30 text-light">Benefits Of Regular AC Service and Maintenance in Discovery Gardens </h2>
-
-                        <div className="row mt-4 gx-4 gy-4">
-                           <div className="col-md-4 mb-2">
-                              <div className="d-flex flex-column align-items-center px-2">
-                                 <div className="mb-3" style={{ width: "80px", height: "80px" }}>
-                                    <img src={getImageSrc('icon/cooling1')} alt="Cooling Efficiency" className="img-fluid rounded-circle border border-4 border-info" />
-                                 </div>
-                                 <h3 className="text-uppercase mb-2 fs-6 text-white">COOLING EFFICIENCY</h3>
-                                 <p className="small">We carry out a complete assessment of the cooling efficiency using a temperature gun.</p>
-                              </div>
-                           </div>
-
-                           <div className="col-md-4 mb-2">
-                              <div className="d-flex flex-column align-items-center px-2">
-                                 <div className="mb-3" style={{ width: "80px", height: "80px" }}>
-                                    <img src={getImageSrc('icon/disinfection')} alt="Disinfect Components" className="img-fluid rounded-circle border border-4 border-warning" />
-                                 </div>
-                                 <h3 className="text-uppercase mb-2 fs-6 text-white">DISINFECT COMPONENTS</h3>
-                                 <p className="small">All hardware components are tested and disinfected, including filters and vents.</p>
-                              </div>
-                           </div>
-
-                           <div className="col-md-4 mb-2">
-                              <div className="d-flex flex-column align-items-center px-2">
-                                 <div className="mb-3" style={{ width: "80px", height: "80px" }}>
-                                    <img src={getImageSrc('icon/fan')} alt="Fan Assessment" className="img-fluid rounded-circle border border-4 border-success" />
-                                 </div>
-                                 <h3 className="text-uppercase mb-2 fs-6 text-white">FAN ASSESSMENT</h3>
-                                 <p className="small">We ensure the fan works correctly with no blockages in the evaporator coil.</p>
-                              </div>
-                           </div>
-
-                           <div className="col-md-4 mb-2">
-                              <div className="d-flex flex-column align-items-center px-2">
-                                 <div className="mb-3" style={{ width: "80px", height: "80px" }}>
-                                    <img src={getImageSrc('icon/thermo')} alt="Thermostat Check" className="img-fluid rounded-circle border border-4 border-primary" />
-                                 </div>
-                                 <h3 className="text-uppercase mb-2 fs-6 text-white">THERMOSTAT CHECK</h3>
-                                 <p className="small">We use a laser temperature gauge to ensure thermostats operate correctly.</p>
-                              </div>
-                           </div>
-
-                           <div className="col-md-4 mb-2">
-                              <div className="d-flex flex-column align-items-center px-2">
-                                 <div className="mb-3" style={{ width: "80px", height: "80px" }}>
-                                    <img src={getImageSrc('icon/airflow')} alt="Airflow Balance" className="img-fluid rounded-circle border border-4 border-danger" />
-                                 </div>
-                                 <h3 className="text-uppercase mb-2 fs-6 text-white">AIRFLOW BALANCE</h3>
-                                 <p className="small">We ensure you get optimal air flow around each room as needed.</p>
-                              </div>
-                           </div>
-
-                           <div className="col-md-4 mb-2">
-                              <div className="d-flex flex-column align-items-center px-2">
-                                 <div className="mb-3" style={{ width: "80px", height: "80px" }}>
-                                    <img src={getImageSrc('icon/customer')} alt="Customer Feedback" className="img-fluid rounded-circle border border-4 border-secondary" />
-                                 </div>
-                                 <h3 className="text-uppercase mb-2 fs-6 text-white">CUSTOMER FEEDBACK</h3>
-                                 <p className="small">Our team provides vital feedback on your AC&apos;s health and any needed air con repair.</p>
-                              </div>
-                           </div>
-                        </div>
-
-                        <div className="text-center mb-4">
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </section>
+            <AcBenefits city="Discovery Garden" />
 
             {/* Why choose us AC */}
             <section className="section cs_py_30">
@@ -518,8 +449,17 @@ const AcRepairInDiscoveryGardensDetail = ({ subtitle, title, reviewsbg, titleSeo
                </div>
             </section>
 
+            <BeforeAfter
+               title="Recent Completed Projects"
+               subTitle="Before & after"
+               beforeImg={`${CDN}/after_img_1/public`}
+               afterTitle="After"
+               afterImg={`${CDN}/before_img_1/public`}
+               beforeTitle="Before"
+            />
+
             {/* We specialise in air conditioning services for the following brands */}
-            <section className="section cs_py_30 bg-light-gray">
+            <section className="section cs_py_30 bg-light-white">
                <div className="container">
                   <h3 className="mb-1 pt-3">EXPRESS & EMERGENCY AC REPAIR SERVICE</h3>
                   <p className="mb-2"><b>Terms & Conditions:</b> There is a callout fee that applies, ranging from AED 195 to 320 depending on capacity, for each diagnosis. Same-day visits are available for bookings made before *12:00 PM. For bookings made after 12:00 PM, next-day visits may be arranged, subject to availability.</p>
@@ -571,25 +511,25 @@ const AcRepairInDiscoveryGardensDetail = ({ subtitle, title, reviewsbg, titleSeo
 
             {/* testimobial section */}
             <Testimonial1
-          subtitle="What Our Clients Say"
-          title="Customer <span>Reviews</span>"
-          bgImg={reviewsbg}
-          testimonialData={testimonial_data}
-          sectionId="home-testimonials"
-        />
+               subtitle="What Our Clients Say"
+               title="Customer <span>Reviews</span>"
+               bgImg={reviewsbg}
+               testimonialData={testimonial_data}
+               sectionId="home-testimonials"
+            />
 
-             <section className="section cs_py_30 bg-dark-blue text-light">
-          <Serviceappointemnt
-          
+            <section className="section cs_py_30 bg-dark-blue text-light">
+               <Serviceappointemnt
+
                   subtitle2="Contact us"
                   title2="Book An Appointment"
                ></Serviceappointemnt>
 
             </section>
-{/* FAQ&apos;s */}
-<section className="section cs_py_30">
-					<div className="container">
-						<h3 className="cs_fs_30">FAQ&apos;s</h3>
+            {/* FAQ&apos;s */}
+            <section className="section cs_py_30">
+               <div className="container">
+                  <h3 className="cs_fs_30">FAQ&apos;s</h3>
                   <div className="cs_accordians_wrapper cs_style_1 p-0">
                      {data.map((item, index) => (
                         <div key={index} className={`cs_accordian cs_style_1 cs_type_1 ${index === openItemIndex ? "active" : ""}`} >
@@ -613,7 +553,7 @@ const AcRepairInDiscoveryGardensDetail = ({ subtitle, title, reviewsbg, titleSeo
                </div>
             </section>
 
-           
+
 
          </div>
       </>
