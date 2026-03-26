0ffaa69f0a75893cf82f5481cffe0fa220ef9faa
Waseem Hassan <faisaljuma01@gmail.com>
2026-03-26 15:23:43 +0400

    service page banner done

--- src/Components/Common/BreadCumbServ.jsx
+++ src/Components/Common/BreadCumbServ.jsx
@@ -168,8 +168,8 @@ const getImageSrc = (imgPath) => {
         `}</style>
       </nav>
       <div className="portfolio-banner-wrap pt-4">
-          <img className='d-none d-md-block' src={getImageSrc('portfoliobanner')} />
-          <img className='d-md-none' src={getImageSrc('portfolio-mobile-banner')} />
+          <img className='d-none d-md-block' src={getImageSrc('service-page-banner')} />
+          <img className='d-md-none' src={getImageSrc('services-mobile-banner')} />
       </div>
     </div>
   );
